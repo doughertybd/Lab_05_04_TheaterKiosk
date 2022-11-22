07be408dfb29e7969e6fa9c3d55a99f48bfd965e
doughertybd <doughebd@mail.uc.edu>
2022-11-22 16:33:31 -0500

    Final commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int personAge = 19;
+        int wristBand = 21;
+            System.out.println("Please enter your age: " +personAge);
+        if (personAge > wristBand){
+            System.out.println("Congrats! you get a wristband.");
+        }
+
+    }
+}
